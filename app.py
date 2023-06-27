c0457b632419019070764419236adbf8acf39dde
assomdevgit <assomggm@gmail.com>
2023-06-27 17:00:57 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+# pip install streamlit
+# steamlit hello
+
+import streamlit as st # streamlit
+
+# st. -> ctrl + space
+
+st.title("나의 파이썬 웹 페이지")
+st.header("수업 8일차에 만들었어요")
+st.subheader("그래도 잘 만들었죠")
+st.write("내가 만든 streamlit 페이지, 너를 위해 구었지!!!")
