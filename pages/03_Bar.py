08f3d275dd5842ed0f485157ab4438bc1b00f986
assomdevgit <assomggm@gmail.com>
2023-06-27 19:33:21 +0900

    common data

--- pages/03_Bar.py
+++ pages/03_Bar.py
@@ -10,29 +10,36 @@ common.page_config()
 
 st.title("2021 Top 10")
 
-df = common.get_2021()
+data_2021 = common.get_2021()
+data_2022 = common.get_2021()
 
+data_2021.columns = data_2021.columns.str.strip()
+data_2022.columns = data_2022.columns.str.strip()
 # genre_counts = df['Genre'].value_counts().sort_values(ascending=False)
+null_list = ['대표국적', '국적', '개봉일', '제작사', '배급사', '등급', '장르', '감독', '배우']
+for i in null_list:
+    data_2021[i] = data_2021[i].fillna('정보없음')
+null_list = ['대표국적', '국적', '개봉일', '제작사', '배급사', '등급', '장르', '감독', '배우']
+for i in null_list:
+    data_2022[i] = data_2022[i].fillna('정보없음')
 
+def numbering(x): # 인트 변환 함수
+    return int(x.replace(',',''))
+
+number_list = ['매출액', '누적매출액', '관객수', '누적관객수', '스크린수'] #인트 변환 적용
+for i in number_list:
+    data_2021[i] = data_2021[i].apply(numbering)
+number_list = ['매출액', '누적매출액', '관객수', '누적관객수', '스크린수'] #인트 변환 적용
+for i in number_list:
+    data_2022[i] = data_2022[i].apply(numbering)
+
+df_2021 = data_2021[['영화명', '개봉일', '매출액', '매출액점유율', '누적매출액', '관객수', '누적관객수', '스크린수', '대표국적', '등급', '대표장르']]
+df_2022 = data_2022[['영화명', '개봉일', '매출액', '매출액점유율', '누적매출액', '관객수', '누적관객수', '스크린수', '대표국적', '등급', '대표장르']]
+df_2021.head()
 tab1, tab2 = st.tabs(["Sales", "Numbers"])
 
 with tab1:
-    # 데이터 예시
-    labels = ['A', 'B', 'C', 'D']
-    values1 = [10, 20, 15, 25]  # 첫 번째 막대 그래프에 표시될 값
-    values2 = [5, 15, 10, 20]  # 두 번째 막대 그래프에 표시될 값
 
-    # 막대 그래프 그리기
-    x = range(len(labels))
-
-    plt.bar(x, values1, width=0.4, align='center', label='Graph 1')
-    plt.bar(x, values2, width=0.4, align='edge', label='Graph 2')
-
-    # x 축 설정
-    plt.xticks(x, labels)
-
-    # 범례 추가
-    plt.legend()
     st.pyplot(plt)
 
 with tab2:
