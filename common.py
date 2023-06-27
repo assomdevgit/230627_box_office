2ca876011da7bc9f18d707503d78467cc61e40e3
assomdevgit <assomggm@gmail.com>
2023-06-27 17:33:17 +0900

    common data

--- common.py
+++ common.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 @st.cache_data
 def get_sales():
-    return pd.read_csv("./KOBIS_2022.csv", encoding="utf-8")
+    return pd.read_csv("./KOBIS_2022.csv", encoding="cp949")
 
 def page_config():
     st.set_page_config(
