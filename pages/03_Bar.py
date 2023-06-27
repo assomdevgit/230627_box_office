b8b57d5ba5736e23fa2288c1af0902007d1b7266
assomdevgit <assomggm@gmail.com>
2023-06-27 19:38:21 +0900

    common data

--- pages/03_Bar.py
+++ pages/03_Bar.py
@@ -41,10 +41,36 @@ for i in number_list:
 df_2021 = data_2021[['영화명', '개봉일', '매출액', '매출액점유율', '누적매출액', '관객수', '누적관객수', '스크린수', '대표국적', '등급', '대표장르']]
 df_2022 = data_2022[['영화명', '개봉일', '매출액', '매출액점유율', '누적매출액', '관객수', '누적관객수', '스크린수', '대표국적', '등급', '대표장르']]
 df_2021.head()
+
+sale_grp_2021 = df_2021.groupby('대표국적')['매출액'].sum().nlargest(20)
+sale_grp_2022 = df_2022.groupby('대표국적')['매출액'].sum().nlargest(20)
+sale_grp_2021
+
 tab1, tab2 = st.tabs(["Sales", "Numbers"])
 
 with tab1:
+    # 데이터 생성
 
+    # 막대 그래프 크기 조절
+    plt.figure(figsize=(12, 4))
+    plt.xlabel('국가')
+    plt.ylabel('매출(단위 억원)')
+    plt.title('국가별 매출 성장률')
+
+    # 막대 너비 설정
+    bar_width = 0.5
+
+    # 첫 번째 데이터 그리기
+    plt.bar(sale_grp_2021.index, sale_grp_2021.values // 100_000_000, width=bar_width, align='center', label='2021')
+
+    # 두 번째 데이터 겹쳐서 그리기
+    plt.bar(sale_grp_2022.index, sale_grp_2022.values // 100_000_000, width=bar_width, align='edge', label='2022')
+
+    # 범례(legend) 표시
+    plt.legend()
+
+    # x축 레이블 기울기 설정
+    plt.xticks(rotation=90)
     st.pyplot(plt)
 
 with tab2:
